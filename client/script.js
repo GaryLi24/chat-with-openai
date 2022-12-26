8ce5f90576ed5ea6580aa118e6e8a0c2aaf3dd0f
liyu24 <307390819@qq.com>
2022-12-26 14:47:03 +0800

    feat:

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async e => {
   loader(messageDiv)
 
   // fetch data from server
-  const response = await fetch('http://localhost:8888', {
+  const response = await fetch('https://chat-with-openai.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
