87ada1d0e3431893126215bbe6ad5409b73482df
liyu24 <307390819@qq.com>
2022-12-26 10:41:00 +0800

    feat: get openai api key

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config()
+dotenv.config() // to use env variables
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -14,7 +14,7 @@ const openai = new OpenAIApi(configuration)
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) // to pass json from the front to backend
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -44,6 +44,6 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () =>
-  console.log('Server is running on port http://localhost:5000')
+app.listen(8888, () =>
+  console.log('Server is running on port http://localhost:8888')
 )
